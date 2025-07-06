8e36392a2efca49fc4d2dbb6fd335c423fcf2b56
Battur Tugsgerel <tbattur22@yahoo.com>
2025-07-06 12:55:29 -0500

    Bumped version for adding debug in Dcitionary

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Hangman2.MixProject do
   def project do
     [
       app: :hangman2,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.14",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
